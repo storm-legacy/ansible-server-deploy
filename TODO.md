4ef0646e4e176aca1d62dadf2479b976ded0e6df
storm-legacy <storm.legacy@disroot.org>
2025-09-03 19:01:31 +0200

    update: moved antivirus script to clamscan

--- TODO.md
+++ TODO.md
@@ -3,8 +3,7 @@
 - [X] Fix IP/User/Port change before rebooting the machine
 - [X] Disable IPv6 by default (ubuntu ignores sysctl parameters)
 - [X] Add user remove list (by username)
-- [X] Configure automatic virus scan
-- [ ] Configure scan to work with `clamdscan` and `clamscan` (interchangeable)
+- [X] Configure automatic virus scan (moved to `clamscan`)
+- [ ] Add general notification script (email/discord webhook)
 - [ ] Configure automatic audits
-- [ ] Configure Zabbix Agent
-- [ ] Configure information sending about found problems (zabbix/other medium)
+- [ ] Configure Zabbix Agent 2
--- roles/clamav/files/usr/local/bin/clamscan_daily.sh
+++ roles/clamav/files/usr/local/bin/clamscan_daily.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -exu
 
-CLAMAV_BIN="/usr/bin/clamdscan"
+CLAMSCAN_BIN="/usr/bin/clamscan"
+
 QUAR_DIR="/var/quarantine/clamav"
 
 LOG_DIR="/var/log/clamav"
@@ -15,12 +16,15 @@ chmod 700 "$QUAR_DIR"
 ls -1t "$LOG_DIR"/*.log 2>/dev/null | tail -n +$((LOG_KEEP+1)) | xargs -r rm -f
 
 # scan
-$CLAMAV_BIN --fdpass --infected --log="$LOG_FILE" --move="$QUAR_DIR" /
+$CLAMSCAN_BIN --exclude='^/dev/' --exclude='^/proc/' --exclude='^/sys/' --exclude='^/run/' --infected --recursive --log="$LOG_FILE" --move="$QUAR_DIR" /
 CLAMSCAN_ERR=$?
 
 if [ "$CLAMSCAN_ERR" -eq "1" ]; then
   # TODO: send information about found malware
+  :
 
 elif [ "$CLAMSCAN_ERR" -gt "1" ]; then
   # TODO: send information about scan error
+  :
+
 fi
